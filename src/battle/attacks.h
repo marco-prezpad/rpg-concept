9ba27227da6666b60664dc57ba1644e74c2ca67c
marco-prezpad <marcoperezpadilla2003@gmail.com>
2025-08-01 16:59:42 +0100

    personjaes

--- src/battle/attacks.h
+++ src/battle/attacks.h
@@ -22,33 +22,6 @@
 #include "bichito/bichito.h"
 
 /**
- * @brief Struct with common attributes for both Bichito and Personaje, it can be included as private attribute and create getters for each attribute
- */
-struct CommonAttributes {
-  std::vector<Type> types_;
-  double attack_;
-  double defense_;
-  double speed_;
-  double critic_prob_;
-  double hp_;
-  double xp_;
-  //std::vector<Effect> effects;
-  //std::vector<State> states;
-
-  // Next lines must be on the public part of Personaje and Bichito classes
-  // std::vector<Type> getTypes() const { return types; }
-  // double getAttackStat() const { return attack; }
-  // double getDefenseStat() const { return defense; }
-  // double getSpeed() const { return speed; }
-  // double getCriticProbability() const { return critic_prob; }
-  // double getHP() const { return hp; }
-  // double getXP() const { return xp; }
-  // const std::vector<Effect>& GetEffects() const { return effects; }
-  // const std::vector<State>& GetStates() const { return states; }
-};
-
-
-/**
  * @brief Virtual class Attack, defines the methods to calculate the damage an attack will do, depending on Entity's attributes
  */
 class Attack {
@@ -59,7 +32,7 @@ class Attack {
  public:
   Attack(double base_damage, std::vector<Type> types) : base_damage_(base_damage), types_(types) {}
   virtual std::string attack_name() const = 0;
-  virtual double Attack::CalculateDamage(std::variant<Bichito>& attacking_entity, std::variant<Bichito>& target_entity) const; // Add Personaje to variant
+  virtual double CalculateDamage(std::variant<Personaje>& attacking_entity, std::variant<Personaje>& target_entity) const; // Add Bichito to variant
   virtual ~Attack() = default;
 };
 
