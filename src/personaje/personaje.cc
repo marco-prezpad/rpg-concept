ce83b6c9d3ce06e24b173c9ebf4c93b4e613dd00
Daniel Alejandro <alu0101595573@ull.edu.es>
2025-08-01 13:56:25 +0200

    Added some changes

---
+++ src/personaje/personaje.cc
@@ -0,0 +1,86 @@
+#include "personaje.h"
+
+#define one_tile 1
+
+void Personaje::CheckMenu() {
+  while (true) {
+    std::cin >> tecla;
+    for (int i = 0; i >= 5; MoverMenu(char tecla))
+      if (tecla == 'x') {
+      // aqui se pondria una funcion que llame a cada opcion del menu
+        if (i == 0) std::cout << "Personaje";
+        if (i == 1) std::cout << "Bichitos";
+        if (i == 2) std::cout << "Items";
+        if (i == 3) std::cout << "Equipment";
+        if (i == 4) std::cout << "Settings";
+      }
+      if (tecla == 'z') break; // me gustaria hacer que fuera escape
+  }    
+}
+
+void Movimiento::MoverMenu(char tecla, int i) {
+  assert(tecla == 'w' || tecla == 's' || tecla == 'x');
+  switch (tecla) {
+    case 'w': i++; break;
+    case 's': assert(i >= 0) i--; break; // para evitar acceder a indices negativos
+  }
+}
+
+void Personaje::CheckItems() {
+  while (true) {
+    std::cin >> tecla;
+    for (int i = 0; i >= this->items_.size(); MoverMenu(char tecla))
+      if (tecla == 'z') break; // me gustaria hacer que fuera escape
+  }
+}
+
+void Personaje::CheckCoords() {
+  assert(this->mov_.GetX() >= 0 && this->mov_.GetY() >= 0);
+  std::cout << this->mov_.GetX() << this->mov_.GetY() << std::endl;
+}
+
+bool Personaje::CheckIteration(const Personaje& other) {
+  if (this->mov_.GetX() + one_tile == other.mov_.GetX() || this->mov_.GetX() - one_tile == other.mov_.GetX()) {
+    return true;
+  }
+  if (this->mov_.GetY() + one_tile == other.mov_.GetY() || this->mov_.GetY() - one_tile == other.mov_.getY()) {
+    return true;
+  }
+  return false;
+}
+
+  // necesitamos clases para cada objeto? o alguna idea para darle atributos a cada cosa sin una clase
+  bool HasHelmet() {
+    if (this->current_helmet_.equipped) { // equipped es un booleano 1 si esta equipado 0 si no
+      return true;
+    }
+    return false;
+  }
+
+  bool HasChestplate() {
+   if (this->current_chestplate_.equipped) { // equipped es un booleano 1 si esta equipado 0 si no
+      return true;
+    }
+    return false;
+  }
+  bool HasPants() {
+     if (this->current_pants_.equipped) { // equipped es un booleano 1 si esta equipado 0 si no
+      return true;
+    }
+    return false;
+  }
+
+  bool HasBoots() {
+     if (this->current_boots_.equipped) { // equipped es un booleano 1 si esta equipado 0 si no
+      return true;
+    }
+    return false;
+  }
+
+  void CheckArmor() {
+    // sumar los atributos
+    if (HasHelmet) this->current_defense_ + this->current_helmet_.defense_;
+    if (HasChestplate) this->current_defense_ + this->current_chestplate_.defense_;
+    if (HasLeggins) this->current_defense_ + this->current_pants_.defense_;
+    if (HasBoots) this->current_defense_ + this->current_boots_.defense_;
+  }
---
+++ src/personaje/personaje.h
@@ -0,0 +1,95 @@
+#ifndef PERSONAJE_H
+#define PERSONAJE_H
+
+#include <string>
+#include <vector>
+
+#include "../movimiento/movimiento.h"
+#include "../coordenadas/coordenadas.h"
+#include "../bichito/bichito.h"
+
+#include "../equipment/accesory.h
+#include "../equipment/weapon.h"
+
+#include "../equipment/helmet.h
+#include "../equipment/chestplate.h
+#include "../equipment/pants.h
+#include "../equipment/boots.h
+
+
+class Personaje {
+ public:
+  const std::string GetName() const { return this->name_; }
+  const double GetAttack()  const { return this->attack_; }
+  const double GetDefense() const { return this->defense_; }
+  const double GetCritChance() const { return this->crit_chance_; }
+  const double GetLife() const { return this->life_; }
+  const double GetSpeed() const { return this->speed_; }
+  const double GetCurrentExp() const { return this->exp_; }
+
+
+  void SetName(const std::string name) { this->name_ = name; }
+  void SetAttack(double attack) { this->attack_ = attack; }
+  void SetDefense(double defense) { this->defense_ = defense; }
+  void SetCritChance(double crit) { this->crit_chance_ = crit; }
+  void SetLife(double life) { this->life_ = life; }
+  void SetSpeed(double speed) { this->speed_ = speed; }
+  void SetExp(double exp) { this->exp_ = exp; }
+ 
+
+
+
+  // void CheckMenu() <-- meter aqui los demas checks (idea)
+  void CheckItems();
+  void CheckBichitos();
+  bool CheckCoords();
+
+  bool CheckIteration();
+
+  
+  bool HasHelmet();
+  bool HasChestplate();
+  bool HasPants();
+  bool HasBoots();
+  void CheckArmor();
+
+
+ private:
+ // atributos generales
+// usar el struct de atributos generales (arreglar)
+  std::string name_;
+  double attack_;
+  double defense_;
+  double personal_crit_chance_;
+  double total_crit_chance_; // critico general arma mas personaje
+  double life_;
+  double speed_;
+  double exp_;
+
+  // atributos especiales
+  std::string type_class_; // podria ser un enum y/o otra clase
+  Accesory accesory_;
+  Helmet current_helmet_;
+  Chestplate current_chestplate_;
+  Pants current_pants_;
+  Boots current_boots_;
+  // estoy pensando en que puede usar dos armas (una por mano)
+  Weapon weapon1_;
+  Weapon weapon2_;
+
+  double current_defense_; // suma de lo que tiene de cada pieza (llamara a funciones que verifiquen si tiene cierta pieza y se sumara a sus estadisticas)
+  std::string afinity_; 
+  bool right_hand_;
+  bool left_hand_;
+
+
+  // gestion de movimiento
+  Movimiento mov_; // movimiento y coordenadas
+
+  // iteractuables (almacenes) 
+  std::vector<Bichito> bichitos_; 
+  std::vector<std::string> items_; // idea: crear una clase que tenga todas las clases para poder meter cualquier cosa aca
+
+};
+
+#endif // PERSONAJE_H
